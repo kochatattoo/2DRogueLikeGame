724ae82c232c165f8c85890e220b926d391bdff2
Kocha <priznavologia@gmail.com>
2024-11-23 19:48:35 +0300

    Напишем класс объединяющий все остальные СинглТоны

--- Knight Adventure/Assets/Scripts/Managers/GameManager.cs
+++ Knight Adventure/Assets/Scripts/Managers/GameManager.cs
@@ -1,29 +1,50 @@
+using Assets.Scripts;
 using UnityEngine;
 
-namespace Assets.Scripts
+public class GameManager : MonoBehaviour
 {
-    public class GameManager : MonoBehaviour
+    public static GameManager Instance { get; private set; }
+
+    public SaveManager saveManager; // Ссылка на SaveManager
+    public AudioManager audioManager; // Ссылка на AudioManager
+    public GUIManager guiManager; // Ссылка на GUIManager
+    public GameInput gameInput; // Ссылка на GameInputManager
+    public User user; // Ссылка на объект User
+
+    private void Awake()
     {
-        public static GameManager Instance { get; private set; }
-
-
-        private void Awake()
+        // Проверка на существование экземпляра GameManager
+        if (Instance == null)
         {
-            if (Instance == null)
-            {
-                Instance = this;
-                DontDestroyOnLoad(this.gameObject);
-                return;
-            }
-            else 
-            Destroy(this.gameObject);
+            Instance = this; // Установка экземпляра
+            DontDestroyOnLoad(gameObject); // Не уничтожать при загрузке новой сцены
+            
         }
-
-        private void Start()
+        else
         {
-
-          
+            Destroy(gameObject); // Удаляем второй экземпляр
         }
-
+        // Инициализация ссылок на синглтоны
+        InitializeSingletons();
     }
+
+    // Метод для инициализации ссылок на синглтоны
+    private void InitializeSingletons()
+    {
+        saveManager = SaveManager.Instance; // Получение ссылки на SaveManager
+        audioManager = AudioManager.Instance; // Получение ссылки на AudioManager
+        guiManager = GUIManager.Instance; // Получение ссылки на GUIManager
+        user = User.Instance; // Получение ссылки на User
+        gameInput = GameInput.Instance; // Получение ссылки на GameInput
+    }
+
+
+    // Вы можете добавить методы, которые обращаются к функционалу других менеджеров
+    public void SaveGame(string fileName)
+    {
+        saveManager.SaveGame(user, fileName);
+    }
+
+    // Другие методы для взаимодействия с другими синглтонами...
+
 }
