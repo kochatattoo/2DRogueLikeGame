d09449c7426ce6f112826bcd8cdb6e8c5d59a461
Kocha <priznavologia@gmail.com>
2024-12-13 16:57:12 +0300

    Теперь предмет поднимается 1 раз и сразу отображается в инвентаре персонажа

--- Knight Adventure/Assets/Scripts/Items/PickableItem.cs
+++ Knight Adventure/Assets/Scripts/Items/PickableItem.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class PickableItem : MonoBehaviour
 {
     public Item item; // Ссылка на наш предмет
+    private bool _canPick=true;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && _canPick)
         {
             Inventory playerInventory = collision.GetComponent<Inventory>();
-            //InventoryUI inventoryUI = collision.GetComponent<InventoryUI>(); // Получаем ссылку на инвентарь UI
+            InventoryUI inventoryUI = FindObjectOfType<InventoryUI>(); // Получаем ссылку на инвентарь UI
+
+            _canPick = false; // Флаг, что бы предотвратить повторное срабатывание
+           
 
             if (playerInventory != null && playerInventory.AddItem(item))
             {
-                //inventoryUI.UpdateInventoryUI(); // Обновляем UI инвентаря
+                if (inventoryUI != null)
+                {
+                    inventoryUI.UpdateInventoryUI(); // Обновляем UI инвентаря
+                }
+                
                 Debug.Log($"Picked up {item.itemName}");
                 Destroy(gameObject); // Удаляем предмет после подбора
             }
             else
             {
                 Debug.Log("Not enough space in inventory!");
+                _canPick=true;
             }
         }
     }
