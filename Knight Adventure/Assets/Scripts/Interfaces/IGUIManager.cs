e97c867aedcced7572089875702934623f9b9ec2
Kocha <priznavologia@gmail.com>
2025-01-07 23:28:53 +0300

    Дополнил интерфейс GUIManager

--- Knight Adventure/Assets/Scripts/Interfaces/IGUIManager.cs
+++ Knight Adventure/Assets/Scripts/Interfaces/IGUIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Interfaces
 {
@@ -10,5 +11,16 @@ namespace Assets.Scripts.Interfaces
     {
         //Метод для старта менеджера  
         void StartManager();
+        void AddQueueWindows();
+        void SetTextAreas();
+        void OpenPlayerWindow(GameObject name);
+        void OpenInformationWindow(int windowIndex);
+        void ShowWindowQueue();
+        void OpenPriorityWindow(int windowIndex);
+        void HandleError(string errorMessage, int numberOfError);
+        void CloseCurrentWindow();
+        void OpenOption();
+        void OpenStorageChestInventory(Inventory chestInventory);
+
     }
 }
