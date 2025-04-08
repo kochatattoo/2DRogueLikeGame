d5aea80ef614ce93ec20a18a8a598ea8a27fe574
Vladislav Kochenkov <priznavologia@gmail.com>
2025-04-08 16:52:08 +0300

    Update PlayState.cs

--- Knight Adventure/Assets/Scripts/StateMachine/PlayState.cs
+++ Knight Adventure/Assets/Scripts/StateMachine/PlayState.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
     public class PlayState : IGameState
     {
@@ -13,6 +13,24 @@
         {
             Debug.Log("Entering Play State");
             // Здесь инициализируем игровое состояние
+			// Включаем игровые менеджеры
+        	var gameInputManager = ServiceLocator.GetService<IGameInput>();
+        	var mapManager = ServiceLocator.GetService<IMapManager>();
+        	var guiManager = ServiceLocator.GetService<IGUIManager>();
+        	var startScreenManager = ServiceLocator.GetService<IStartScreenManager>();
+
+        	if (gameInputManager is MonoBehaviour gameInputScript) gameInputScript.gameObject.SetActive(true);
+        	if (mapManager is MonoBehaviour mapScript) mapScript.gameObject.SetActive(true);
+        	if (guiManager is MonoBehaviour guiScript) guiScript.gameObject.SetActive(true);
+        	if (startScreenManager is MonoBehaviour startScreenScript) startScreenScript.gameObject.SetActive(true);
+
+        	gameInputManager.StartManager();
+        	mapManager.StartManager();
+        	guiManager.StartManager();
+        	startScreenManager.StartManager();
+
+        	var audioManager = ServiceLocator.GetService<IAudioManager>();
+        	audioManager.InitializePlayerAudio();
         }
 
         public void Update()
@@ -24,6 +42,15 @@
         {
             Debug.Log("Exiting Play State");
             // Здесь освобождаем ресурсы, если это необходимо
+			var gameInputManager = ServiceLocator.GetService<IGameInput>();
+			var mapManager = ServiceLocator.GetService<IMapManager>();
+			var guiManager = ServiceLocator.GetService<IGUIManager>();
+			var startScreenManager = ServiceLocator.GetService<IStartScreenManager>();
+
+		   if (gameInputManager is MonoBehaviour gameInputScript) gameInputScript.gameObject.SetActive(false);
+   	       if (mapManager is MonoBehaviour mapScript) mapScript.gameObject.SetActive(false);
+           if (guiManager is MonoBehaviour guiScript) guiScript.gameObject.SetActive(false);
+           if (startScreenManager is MonoBehaviour startScreenScript) startScreenScript.gameObject.SetActive(false);
         }
     }
 
